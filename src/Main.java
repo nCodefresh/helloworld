2f054cb0bbdefcbd0efcf5ea1cf440a6a54f49b3
nikolai <n.ivanov.p@gmail.com>
2016-06-21 09:50:53 +0300

    list of env vars

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        System.out.println("List of env variables");
+
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n",
+                    envName,
+                    env.get(envName));
+        }
     }
+
 }
